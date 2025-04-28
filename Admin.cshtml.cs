0371fda67f5cba4e86ed8935ea6503dd67dcd540
Sheikh Sajit <114417876+sheikhsajit@users.noreply.github.com>
2025-04-29 00:23:35 +0600

    Add files via upload

---
+++ Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Garments_Management_System.Views.GMS
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
